498b4a5960cca04d420c4ce71bc4bf14f3c58aa5
Dan <36653211+Danixcviii@users.noreply.github.com>
2019-01-31 10:29:55 -0500

    Update Flexi.java

--- data/Flexi.java
+++ data/Flexi.java
@@ -3,15 +3,21 @@ package data;
 class Flexi extedns Pasaje implements Reembolsable{
 
   public static final double MULTA = 70000;
+  public static final string NO_SILLA = "Sin Asignar";
   
-  public Flexi (String id,double valor,String silla){
+  public Flexi (String id,double valor){
     super(id,valor);
+    setSilla(NO_SILLA);
+  }
+  
+  public void setSilla(String silla){
     this.silla = silla;
   }
   
   public String getSilla(){
     return silla;
   }
+  
   
   public double calcularReembolso(){
     return valor - MULTA;
