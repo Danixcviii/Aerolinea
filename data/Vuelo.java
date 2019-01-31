927178ed802d2af3650da2daf0cd2754a47878c4
Dan <36653211+Danixcviii@users.noreply.github.com>
2019-01-31 09:54:55 -0500

    Create Vuelo.java

---
+++ data/Vuelo.java
@@ -0,0 +1,11 @@
+package data;
+
+class Vuelo {
+
+  private String codigo;
+  private LocalDate fecha;
+  private int capacidad;
+  private int disponiblesPromo;
+  private int disponiblesEcono;
+  private int disponiblesFlexi;
+}
