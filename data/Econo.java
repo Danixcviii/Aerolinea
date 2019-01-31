bcf1e8143e7e92e4827658c6140d6ce0ef5271f9
Dan <36653211+Danixcviii@users.noreply.github.com>
2019-01-31 10:08:14 -0500

    Update Econo.java

--- data/Econo.java
+++ data/Econo.java
@@ -1,5 +1,10 @@
 package data;
 
 class Econo extends Pasaje implements Reembolsable{
-
+  
+  public static final double MULTA = 100000;
+  
+  public double calcularReembolso(){
+    return valor - MULTA;
+  }
 }
