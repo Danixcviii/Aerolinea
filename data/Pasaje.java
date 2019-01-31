f877c2cc29bbcf4e438a86b77ebcd03d5aa41414
Dan <36653211+Danixcviii@users.noreply.github.com>
2019-01-31 10:17:03 -0500

    Update Pasaje.java

--- data/Pasaje.java
+++ data/Pasaje.java
@@ -2,6 +2,11 @@ package data;
 
 class pasaje {
   
+  public Pasaje(String id,double valor,String nombrePasajero){
+    this.id = id;
+    this.valor = valor;
+    this.nombrePasajero = nombrePasajero;
+  }
   
   protected String id;
   protected double valor;
