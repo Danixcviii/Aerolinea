2d2723783d71468d0ea3cb7b095f353102610d97
Dan <36653211+Danixcviii@users.noreply.github.com>
2019-01-31 10:10:31 -0500

    Update Promo.java

--- data/Promo.java
+++ data/Promo.java
@@ -2,5 +2,10 @@ package data;
 
 class Promo extends Pasaje implements Reembolsable {
 
+  public static final double MULTA = 70000;
   
+  public double calcularReembolso(){
+    return valor - MULTA;
+  }
+
 }
