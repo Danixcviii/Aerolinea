1c99104ad780e7f59004d099ee01df8778d8a053
Dan <36653211+Danixcviii@users.noreply.github.com>
2019-01-31 10:26:44 -0500

    Update Pasaje.java

--- data/Pasaje.java
+++ data/Pasaje.java
@@ -2,10 +2,29 @@ package data;
 
 class pasaje {
   
-  public Pasaje(String id,double valor,String nombrePasajero){
+  public static final String NO_PASAJERO = "Sin Asignar";
+  
+  public Pasaje(String id,double valor){
     this.id = id;
     this.valor = valor;
+    setNombrePasajero(NO_PASAJERO);
+    
+  }
+  
+  public void setNombrePasajero(String nombrePasajero){
     this.nombrePasajero = nombrePasajero;
+  }
+  
+  public String getID(){
+    return id;
+  }
+  
+  public double getValor(){
+    return valor;
+  }  
+  
+  public String getNombrePasajero(){
+    return nombrePasajero;
   }
   
   protected String id;
